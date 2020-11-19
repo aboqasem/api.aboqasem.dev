b66b079a7601f72451a667d68c0599d043ba0938
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-11-19 10:40:49 +0800

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv')
+  .config();
+
+const indexRouter = require('./routes/index');
+const articlesRouter = require('./routes/articles');
+const projectsRouter = require('./routes/projects');
+const middlewares = require('./middlewares');
+
+const app = express();
+
+mongoose.connect(process.env.DB || 'mongodb://localhost/portfolio', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(morgan('common'));
+app.use(helmet());
+app.use(cors({
+  origin: process.env.CORS_ORIGIN || 'http://localhost',
+}));
+
+app.use('/', indexRouter);
+app.use('/api/articles', articlesRouter);
+app.use('/api/projects', projectsRouter);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  if (process.env.NODE_ENV === 'development') {
+    console.log(`Listening on http://localhost:${port}`);
+  }
+});
---
+++ src/routes/articles.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const router = express.Router();
+
+const Article = require('../models/Article');
+
+router.get('/', async (req, res, next) => {
+  await Article.find({}, (error, articles) => {
+    if (!error) {
+      return res.json(articles);
+    }
+    return next(error);
+  });
+});
+
+router.post('/', async (req, res, next) => {
+  const article = new Article(req.body);
+  await article.save((error) => {
+    if (!error) {
+      return res.json(article);
+    }
+    if (error.name === 'ValidationError') {
+      res.status(422);
+    }
+    return next(error);
+  });
+});
+
+module.exports = router;
