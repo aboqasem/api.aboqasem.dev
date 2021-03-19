20f2a4815fdf80ed91658873b573929712274c09
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-03-19 13:23:02 +0800

    Cache posts for 20mins

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,29 +1,41 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import Post from '../models/Post';
 
 const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  await Post.find({}, (error, posts) => {
-    if (!error) {
-      return res.json(posts);
-    }
-    return next(error);
-  });
-});
+const cachingTime = 20 * 60 * 1000;
+let cacheTime: number;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+let cachedPosts: mongoose.Document<any, Record<string, unknown>>[];
 
-router.post('/', async (req, res, next) => {
-  if (req.body.secretKey !== process.env.SECRET_KEY) return res.json({ message: 'Unauthorized access!' });
-  const post = new Post(req.body);
-  return post.save((error) => {
-    if (!error) {
-      return res.json(post);
+router
+  .get('/', (req, res, next) => {
+    if (cacheTime && Date.now() - cacheTime < cachingTime) {
+      return res.json(cachedPosts);
     }
-    if (error.name === 'ValidationError') {
-      res.status(422);
-    }
-    return next(error);
+
+    return Post.find({}, (error, newPosts) => {
+      if (!error) {
+        cacheTime = Date.now();
+        cachedPosts = newPosts;
+        return res.json(newPosts);
+      }
+      return next(error);
+    });
+  })
+  .post('/', (req, res, next) => {
+    if (req.body.secretKey !== process.env.SECRET_KEY) return res.json({ message: 'Unauthorized access!' });
+    const post = new Post(req.body);
+    return post.save((error) => {
+      if (!error) {
+        return res.json(post);
+      }
+      if (error.name === 'ValidationError') {
+        res.status(422);
+      }
+      return next(error);
+    });
   });
-});
 
 export default router;
