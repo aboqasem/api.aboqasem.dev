d1b14fde54ec9a356f29cd8c445bb007a31bb844
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2021-03-23 08:36:23 +0800

    Better PostSchema

--- src/models/Post/index.ts
+++ src/models/Post/index.ts
@@ -1,26 +1,27 @@
 import mongoose, { Schema } from 'mongoose';
 import { IPost, IPostDocument, IPostModel } from './types';
 
-const postSchemaDefinition: Record<keyof IPost, unknown> = {
-  title: {
-    type: String,
-    required: true,
+const PostSchema = new Schema<IPostDocument, IPostModel, IPost>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: false,
+    },
   },
-  content: {
-    type: String,
-    required: true,
+  {
+    timestamps: true,
   },
-  img: {
-    type: String,
-    required: false,
-  },
-};
+);
 
-const postSchema = new Schema<IPostDocument, IPostModel>(postSchemaDefinition, {
-  timestamps: true,
-});
-
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model('Post', PostSchema);
 
 export * from './types';
 
